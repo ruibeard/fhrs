dcd3646f48fa24669ad6e73b86e5dc19fb49d5f2
Rui Almeida <ruitiagoribeiroalmeida@gmail.com>
2022-10-24 17:30:41 +0100

    Create Establishment Migration

---
+++ database/migrations/2022_10_26_114532_create_establishments_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     *
+     *
+     *
+     *
+     *
+     */
+    public function up()
+    {
+        Schema::create('establishments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('FHRSID')->unique()->nullable();
+            $table->integer('LocalAuthorityBusinessID')->nullable();
+            $table->string('BusinessName')->nullable();
+            $table->integer('BusinessTypeID')->nullable();
+            $table->string('AddressLine1')->nullable();
+            $table->string('AddressLine2')->nullable();
+            $table->string('AddressLine3')->nullable();
+            $table->string('AddressLine4')->nullable();
+            $table->string('PostCode')->nullable();
+            $table->string('RatingValue')->nullable();
+            $table->string('RatingDate')->nullable();
+            $table->string('LocalAuthorityName')->nullable();
+            $table->unsignedTinyInteger('Hygiene')->nullable();
+            $table->unsignedTinyInteger('Structural')->nullable();
+            $table->unsignedTinyInteger('ConfidenceInManagement')->nullable();
+            $table->string('longitude')->nullable();
+            $table->string('latitude')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('establishments');
+    }
+};
